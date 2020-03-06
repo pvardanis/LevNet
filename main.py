0c21bcc1d656eb22880f04daec5429f982659736
Panagiotis Vardanis <panagiotisvardanis@gmail.com>
2020-03-06 13:13:08 +0000

    added tensorboard for colab

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main(config):
         return
 
     transform = transforms.Compose([#transforms.RandomRotation(30),
-                                    transforms.RandomResizedCrop(224), # comment this if using Tester
+                                    # transforms.RandomResizedCrop(224), # comment this if using Tester
                                     # transforms.RandomHorizontalFlip(),
                                     # transforms.RandomVerticalFlip(),
                                     transforms.ToTensor(),
