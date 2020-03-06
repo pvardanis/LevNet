c373363814dbfb488933b7abb9da4d2d846b2e4e
Panagiotis Vardanis <panagiotisvardanis@gmail.com>
2020-03-06 01:34:38 +0000

    added print option

--- solver.py
+++ solver.py
@@ -103,8 +103,11 @@ class Solver(object):
     
     def train(self):
         m = RunManager(self.save_best_model, self.stop_early)
+        if global_vars.console: 
+            global_vars.cls() 
+        else: 
+            if global_vars.colab: output.clear() else clear_output(wait=True)
         for run in RunBuilder.get_runs(self.params):
-            global_vars.cls()
             network = self.build_model().to(self.device) # this returns a new instance of the network .to(self.device)
             train_loader = torch.utils.data.DataLoader(self.train_set, num_workers=self.num_workers, batch_size=run.batch_size, shuffle=True)
             valid_loader = torch.utils.data.DataLoader(self.valid_set, num_workers=self.num_workers, batch_size=run.batch_size, shuffle=True)
