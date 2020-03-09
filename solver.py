3186622e28c6fb9373fc5fba06bce717d00038cb
Panagiotis Vardanis <panagiotisvardanis@gmail.com>
2020-03-09 16:12:39 +0000

    created data.h5 fixed CustomDataset()

--- solver.py
+++ solver.py
@@ -107,6 +107,8 @@ class Solver(object):
                 print('\nTrain:\n')
                 for batch_idx, (images, labels) in enumerate(Bar(loaders['train'])):
                     images, labels = images.to(self.device), labels.to(self.device)
+                    inputs = Variable(images)
+                    print(inputs.is_cuda) # returns true   
                     optimizer.zero_grad()
                     preds = network(images)
                     loss = self.criterion(preds, labels)
