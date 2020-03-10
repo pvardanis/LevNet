afbd118620465d752c03c35315de9f6bd2610b5e
Panagiotis Vardanis <panagiotisvardanis@gmail.com>
2020-03-10 00:14:08 +0000

    test

--- helpers.py
+++ helpers.py
@@ -309,9 +309,9 @@ class CustomDataset(Dataset):
         self.path = path
         self.num_files = len(os.listdir(self.path+'/phases'))
         self.file = h5py.File(self.path+'/data.h5', 'r')
-        self.transform = transforms.Compose([transforms.ToTensor(),
-                                        transforms.Normalize([0.485, 0.456, 0.406],
-                                        [0.229, 0.224, 0.225])])
+        self.transform = transforms.Compose([transforms.ToTensor()])#,
+                                        # transforms.Normalize([0.485, 0.456, 0.406],
+                                        # [0.229, 0.224, 0.225])])
 
 
     def __getitem__(self, index):
