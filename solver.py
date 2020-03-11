883237cdc1892a600c68c326a79e091106bd95fc
Panagiotis Vardanis <panagiotisvardanis@gmail.com>
2020-03-11 16:05:15 +0000

    added lr scheduler

--- solver.py
+++ solver.py
@@ -125,7 +125,7 @@ class Solver(object):
             for epoch in range(self.num_epochs):
                 # Update lr to dataframe
                 if self.lr_scheduler:
-                    m.run_params['lr'] = optimizer.param_groups[0]['lr']  
+                    print(m.run_params)# = optimizer.param_groups[0]['lr']  
 
                 # Train
                 network.train() # keep grads
