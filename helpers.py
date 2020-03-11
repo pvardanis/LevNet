b95df1a60759e6885ba943515438b6cdc70f4ee9
Panagiotis Vardanis <panagiotisvardanis@gmail.com>
2020-03-11 13:28:26 +0000

    updated cosine loss, changed output to 1024

--- helpers.py
+++ helpers.py
@@ -389,8 +389,9 @@ def Cosine(output, target):
     squares = output ** 2 # (x ^ 2, y ^ 2)
     loss_1 = ((squares[::2] + squares[1::2]) - 1) ** 2 # (x ^ 2 + y ^ 2 - 1) ** 2
 
-    print(output[1::2].shape, output[::2].shape)
     # Compute the second loss, 1 - cos
-    # loss_2 =  1. - torch.cos(torch.atan2(output[1::2], output[::2]) - target)  
+    y = output[1::2]
+    x = output[::2]
+    loss_2 =  1. - torch.cos(torch.atan2(y, x) - target)  
     
-    return torch.mean(loss_1)# + torch.mean(loss_2)
+    return torch.mean(loss_1) + torch.mean(loss_2)
